77a3b226bc5279b87ff3a7c5f7b60333bd42419b
Viswanath <viswa20.2001@gmail.com>
2025-01-02 18:16:35 +0530

    Build checkout page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { ShopContext } from '../context/ShopContext'
 const Navbar = () => {
 
   const [visible, setVisible] = useState(false)
-  const { showSearch,setShowSearch } = useContext(ShopContext)
+  const { showSearch,setShowSearch, getCartCount } = useContext(ShopContext)
     
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
@@ -44,7 +44,7 @@ const Navbar = () => {
             </div>
             <Link to='/cart' className='relative'> 
                 <img className='w-5 min-w-5' src={assets.cart_icon} alt="" />
-                <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
+                <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>{getCartCount()}</p>
             </Link>
             <img onClick={()=>setVisible(true)} className='w-5 cursor-pointer sm:hidden' src={assets.menu_icon} alt="" />
         </div>
